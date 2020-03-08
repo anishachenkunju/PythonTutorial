dcfc6e58cc7356decee8996bedf1cf82fdd6f8b1
anisha <anisha@PUNAnishaMac.local>
2020-03-08 12:04:16 +0530

    commit

--- 4.py
+++ 4.py
@@ -21,6 +21,10 @@ def translate(word):
 
 word  = input('enter a word:')
 output = translate(word)
-for item in output:
-    print(item)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
 
